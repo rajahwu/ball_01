1e40047749abf16df34e53f938005d011ab9d837
rajahwu <vmradford@gmail.com>
2022-11-28 23:02:49 -0500

    add page links

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 const signUpBtn = document.getElementById('sign_up-btn');
-signUpBtn.addEventListener("click", (e) => e.preventDefault())
+signUpBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert('Thanks for Signing Up')
+})
